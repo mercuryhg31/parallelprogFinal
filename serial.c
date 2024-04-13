461e4ecee74b60439ddb38ef6c2abf7c3d2631be
Harshita Garg <26150304+mercuryhg31@users.noreply.github.com>
2024-04-13 17:42:18 -0400

    .

---
+++ serial.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define HEIGHT height;
+#define WIDTH width;
+#define MAX_ITER max_iterations;
+
+
+
+
+int main(int argc, char* argv[]) {
+	int height = atoi()
+    
+    /** ARGS:
+	 * 1) size (height, width)
+	 * 2) num iterations
+	*/
+	
+}
